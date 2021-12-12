00c25d3c8fd8bc2031a2022ba248316b61201f51
dhruv upadhyay <dhruv.upadhyay1998@gmail.com>
2021-12-11 19:37:41 -0500

    added all field for better result

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def make_summary(query,pois,langs,countries,sentiment,start):
     hashtags = {}
     mentions = {}
     lines = query
-    url1 = 'http://3.135.204.148:8983/solr/'
-    url2 = '/select?&defType=edismax&facet.field=tweet_lang&facet.field=country&facet.field=sentiment&facet=true&'
+    url1 = 'http://18.118.137.90:8983/solr/'
+    url2 = '/select?&defType=edismax&facet.field=tweet_lang&facet.field=country&facet.field=sentiment&facet.field=hashtags&facet=true&'
     if len(pois) > 0:
         url2 += "fq=" + get_url_by_field(pois, "poi_name") + "&"
     if len(langs) > 0:
@@ -41,11 +41,12 @@ def make_summary(query,pois,langs,countries,sentiment,start):
         url2 += "fq=" + get_url_by_field(countries, "country") + "&"
     if len(sentiment) > 0:
         url2 += "fq=" + get_url_by_field(sentiment, "sentiment") + "&"
-    url2 += 'qf=text_en^3&qf=tweet_text^1&q.op=OR&q='
+    url2 += 'qf=text_en^1&qf=all^3&q.op=OR&q='
     url3 = '&wt=json&indent=true&rows=10&start=' + start
-    models = ["IR_Project4"]
+    models = ["IR_Final1"]
     lang1 = "text_en%3A"
-    lang2 = "text_text%3A"
+    # lang2 = "text_text%3A"
+    lang2 = "all%3A"
     # lang3 = "text_hi%3A"
     OR = "%20or%20"
     for model in models:
@@ -69,10 +70,6 @@ def make_summary(query,pois,langs,countries,sentiment,start):
                         mentions[m] += 1
                     else:
                         mentions[m] = 1
-    sorted_hashtags = sorted(hashtags.items(), key=lambda item: item[1], reverse=False)
-    data["hashtags"] = {k: v for k, v in sorted_hashtags}
-    sorted_mentions = sorted(mentions.items(), key=lambda item: item[1], reverse=False)
-    data["mentions"] = {k: v for k, v in sorted_mentions}
     return data
 
 @app.route('/api',methods=["POST"])
@@ -92,14 +89,12 @@ def my_microservice():
     return json.dumps(k,ensure_ascii=False, indent=4,sort_keys=True)
 
 
-@app.route('/test', methods=['GET'])
-def test():
-    return "TEST"
-
-
 if __name__ == '__main__':
     app.config['RESTFUL_JSON'] = {
         'ensure_ascii': True
     }
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=9999, debug=True)
 
+# @app.route('/test', methods=['GET'])
+# def test():
+#     return "TEST"
